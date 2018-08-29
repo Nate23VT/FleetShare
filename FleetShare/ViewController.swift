9abe789cd62e35cbe0c6951a84b92de19f0e2ddb
Nathan Worcester <nathanworcester@nikkis-imac.lan>
2018-08-28 21:04:10 -0400

    Initial Commit

---
+++ FleetShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FleetShare
+//
+//  Created by Nathan Worcester on 8/28/18.
+//  Copyright © 2018 Nathan Worcester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
